9b4f6fd2f8dfa408b205209c0290e337fc0de32a
rezonmain <adiazgomez101@gmail.com>
2022-09-22 02:06:06 -0700

    styling

--- src/components/LinearTuningLane/LinearTuningLane.tsx
+++ src/components/LinearTuningLane/LinearTuningLane.tsx
@@ -17,12 +17,19 @@ const LinearTuner = ({ data }: { data: TunerData | undefined }) => {
 	});
 
 	return (
-		<div className='h-full w-3/4 mx-auto flex flex-col justify-center gap-14'>
-			<div className='text-center text-stone-500 text-xl'>
+		<div
+			id='tuner-container'
+			className='h-full w-3/4 mx-auto flex flex-col justify-center gap-14'
+		>
+			<div id='freq-indicator' className='text-center text-stone-500 text-xl'>
 				{data ? <p>{data.frequency.toFixed(2)} hz</p> : <p>- hz</p>}
 			</div>
-			<div className='flex flex-row justify-between items-center relative'>
+			<div
+				id='lane-container'
+				className='flex flex-row justify-between items-center relative'
+			>
 				<animated.span
+					id='cents-indicator'
 					style={tunerIndicatorProps}
 					className='inline-block absolute mb-16 -translate-x-1/2 text-center text-stone-500'
 				>
@@ -32,23 +39,26 @@ const LinearTuner = ({ data }: { data: TunerData | undefined }) => {
 				<div className='outline outline-stone-300 h-7 aspect-square rounded-full'></div>
 				<div className='border border-stone-300 w-full h-0'></div>
 				<animated.span
+					id='tuning-indicator'
 					style={{
 						...tunerIndicatorProps,
 						backgroundColor: `hsl(${
 							data ? -Math.abs(data!.cents * 3) + 142 : 142
-						},69%, 58%)`,
+						}, 69%, 58%)`,
 					}}
 					className='z-10 absolute inline-block aspect-square h-7 rounded-full -translate-x-1/2 transition-colors'
 				></animated.span>
 			</div>
-			<animated.div style={noteIndicatorProps} className='self-center'>
+			<animated.div
+				id='note-indicator'
+				style={noteIndicatorProps}
+				className='self-center'
+			>
 				{data ? (
-					<div className='flex flex-row items-center rotate-45 justify-between gap-4'>
-						<p className='text-5xl -rotate-45 block'>
-							{data.noteName}
-							<sub className='text-xl'>{data.octave}</sub>
-						</p>
-					</div>
+					<p className='text-5xl block'>
+						{data.noteName}
+						<sub className='text-xl'>{data.octave}</sub>
+					</p>
 				) : (
 					<div className='h-12'></div>
 				)}
