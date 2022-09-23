003f7fe62d84cfa482c970fb3f8a5951163a9fbe
rezonmain <adiazgomez101@gmail.com>
2022-09-22 23:04:49 -0700

    fixed header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import { BsGear } from 'react-icons/bs';
 const Header = () => {
 	return (
-		<header className='fixed top-0 left-0 w-full h-fit p-5 flex flex-row items-center justify-between text-stone-300'>
-			<div id='icon'>
-				<span className='text-3xl'>nertu</span>
-			</div>
-			<div className='cursor-pointer hover:rotate-45 transition-transform'>
-				<BsGear size={30} />
+		<header className='fixed top-0 w-full h-fit p-5  text-stone-300'>
+			<div className='sm:max-w-3xl xl:px-0 xl:max-w-5xl mx-auto flex flex-row items-center justify-between'>
+				<div id='icon'>
+					<span className='text-3xl'>nertu</span>
+				</div>
+				<div className='cursor-pointer hover:rotate-45 transition-transform'>
+					<BsGear size={30} />
+				</div>
 			</div>
 		</header>
 	);
