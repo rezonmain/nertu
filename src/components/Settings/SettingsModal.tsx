7a9270795e2decad351c182087511abad475b348
rezonmain <adiazgomez101@gmail.com>
2022-09-23 18:32:00 -0700

    more modal styling

--- src/components/Settings/SettingsModal.tsx
+++ src/components/Settings/SettingsModal.tsx
@@ -30,12 +30,13 @@ const SettingsModal = ({
 						}}
 						className='w-full absolute top-0 left-0 max-w-[640px] sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 bg-black z-30 sm:border-[0.5px] border-stone-600'
 					>
-						<div id='modal-content' className='p-7'>
+						<div id='modal-content' className='px-7'>
 							<div
-								className='top-7 sticky cursor-pointer w-fit h-fit mb-5'
+								className='top-0 sticky cursor-pointer w-fit h-fit mb-5 flex flex-row bg-black py-5 gap-8'
 								onClick={onSettings}
 							>
-								<IoMdClose size={38} />
+								<IoMdClose size={30} className='translate-y-[3px]' />
+								<h2 className='text-2xl'>settings</h2>
 							</div>
 							<div className=''>{children}</div>
 						</div>
