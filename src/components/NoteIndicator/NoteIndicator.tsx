607f2c2986972396fef935405c14f123c4e1d948
rezonmain <adiazgomez101@gmail.com>
2022-09-25 12:49:35 -0700

    added show enharmonic option to NoteIndicator component

--- src/components/NoteIndicator/NoteIndicator.tsx
+++ src/components/NoteIndicator/NoteIndicator.tsx
@@ -1,5 +1,6 @@
 import { useSpring, animated } from '@react-spring/web';
 import TET, { NoteName } from '../../lib/classes/TET';
+import { useSettings } from '../../lib/context/settingsContext';
 
 const NoteIndicator = ({
 	note,
@@ -10,6 +11,7 @@ const NoteIndicator = ({
 	octave: number | undefined;
 	as?: 'linear' | 'curved';
 }) => {
+	const { settings } = useSettings();
 	const noteIndicatorProps = useSpring({
 		to: {
 			opacity: note ? 1 : 0,
@@ -27,14 +29,14 @@ const NoteIndicator = ({
 		>
 			<div
 				className={`flex flex-col gap-7 -rotate-45 ${
-					enharmonic ? '-translate-y-3' : ''
+					enharmonic && settings.showEnharmonic && '-translate-y-3'
 				}`}
 			>
 				<p className='text-5xl block rotate-45 font-music'>
 					{note}
 					<sub className='text-xl'>{octave}</sub>
 				</p>
-				{enharmonic && (
+				{enharmonic && settings.showEnharmonic && (
 					<>
 						<hr className='border-stone-500' />
 						<p className='text-4xl block rotate-45 text-stone-500 translate-x-4 -translate-y-1 font-music'>
